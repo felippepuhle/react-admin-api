ae958ca7ddb5c8d7fadaa3c2fe835c30330a6e1e
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2016-10-13 20:33:58 -0300

    Initial commit

---
+++ src/main/java/br/com/felippepuhle/ReactadminApplication.java
@@ -0,0 +1,12 @@
+package br.com.felippepuhle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactadminApplication.class, args);
+	}
+}
